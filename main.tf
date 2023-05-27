5fed704cd481198842be087e453a00ae08d53621
somnathmukherji <99045348+somnathmukherji@users.noreply.github.com>
2023-05-28 02:53:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ resource "aws_instance" "TERRAGRUNT1" {
   #key_name                    = "${var.instance-key-name != "" ? var.instance-key-name : ""}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
   # user_data                   = "${file("${var.user-data-script}")}"
-  user_data                   = "${var.user-data-script != "" ? file("${var.user-data-script}") : ""}"
+ # user_data                   = "${var.user-data-script != "" ? file("${var.user-data-script}") : ""}"
   vpc_security_group_ids      = ["${aws_security_group.sg.id}"]
   subnet_id                   = "${aws_subnet.subnet.id}"
 
   tags = {
-    Name = "${var.instance-tag-name}"
+    Name = "${var.tags}"
   }
 }
 
