696c91f30d7c02a4cf7a93e3c7f8194ab553bf74
somnathmukherji <99045348+somnathmukherji@users.noreply.github.com>
2023-05-28 00:29:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,19 +11,19 @@ variable "aws-profile" {
 variable "instance-ami" {
   description = "The AMI (Amazon Machine Image) that identifies the instance"
   type        = string
-  default     = ami-081a91961c85e3640
+  default     = "ami-081a91961c85e3640"
 }
 
 variable "instance-type" {
   description = "The instance type to be used"
   type        = string
-  default     = t2.micro
+  default     = "t2.micro"
 }
 
 variable "instance-key-name" {
   description = "The name of the SSH key to associate to the instance. Note that the key must exist already."
   type        = string
-  default     = prodkey
+  default     = "prodkey"
 }
 
 variable "iam-role-name" {
@@ -53,35 +53,35 @@ variable "instance-tag-name" {
 variable "vpc-cidr-block" {
   description = "The CIDR block to associate to the VPC"
   type        = string
-  default     = 10.0.0.0/16
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet-cidr-block" {
   description = "The CIDR block to associate to the subnet"
   type        = "string
-  default     = 10.0.1.0/24
+  default     = "10.0.1.0/24"
 }
 
 variable "vpc-tag-name" {
   description = "The Name to apply to the VPC"
   type        = string
-  default     = my_vpc
+  default     = "my_vpc"
 }
 
 variable "ig-tag-name" {
   description = "The name to apply to the Internet gateway tag"
   type        = string
-  default     = my_igw
+  default     = "my_igw"
 }
 
 variable "subnet-tag-name" {
   description = "The Name to apply to the VPN"
   type        = string
-  default     = public_subnet_1
+  default     = "public_subnet_1"
 }
 
 variable "sg-tag-name" {
   description = "The Name to apply to the security group"
   type        = string
-  default     = nat_sg
+  default     = "nat_sg"
 }
